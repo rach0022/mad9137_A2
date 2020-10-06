cbfff018751eeccd27c20dc1132c45f8cb266aa1
RaviC Rachamalla <rach0022@algonquinlive.com>
2020-10-06 18:02:21 -0400

    initial commit for xcode assignment 2

---
+++ Assignment_02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment_02
+//
+//  Created by Ravi Rachamalla on 2020-10-06.
+//  Copyright © 2020 rav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
