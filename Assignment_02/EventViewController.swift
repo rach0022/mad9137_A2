24c592fccbca203b9d0242b3e6f4cf3b06213a34
RaviC Rachamalla <rach0022@algonquinlive.com>
2020-10-06 19:07:54 -0400

    finished pass 1 at Storyboard REQ

---
+++ Assignment_02/EventViewController.swift
@@ -0,0 +1,33 @@
+//
+//  EventViewController.swift
+//  Assignment_02
+//
+//  Created by Ravi Rachamalla on 2020-10-06.
+//  Copyright © 2020 rav. All rights reserved.
+//
+
+import UIKit
+
+class EventViewController: UIViewController {
+
+    @IBOutlet weak var TitleTextLabel: UILabel!
+    @IBOutlet weak var SubtitleTextLabel: UILabel!
+    @IBOutlet weak var DescriptionTextView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
